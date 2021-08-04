44a6123be12521d45657179dfe4f0254eddacad2
jh Lim <jhwlim25@gmail.com>
2021-08-04 22:56:53 +0900

    :wrench:chore: Vuex 설치

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import router from "./router";
+import store from "./store";
 import vuetify from "./plugins/vuetify";
 import axios from "./plugins/axios";
 import App from "./App.vue";
@@ -7,6 +8,7 @@ import App from "./App.vue";
 const app = new Vue({
     el: '#app',
     router,
+    store,
     vuetify,
     render: h => h(App),
 });
