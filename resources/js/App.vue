bffe9e71651b5c4d8e2de8224a2acdcd97216103
jh Lim <jhwlim25@gmail.com>
2021-08-11 01:25:23 +0900

    :sparkles:feat: AppAlert 컴포넌트 추가

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,19 +1,23 @@
 <template>
     <v-app>
+        <app-alert></app-alert>
         <app-header></app-header>
         <v-main class="grey lighten-2">
             <router-view></router-view>
+
         </v-main>
     </v-app>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader";
+import AppAlert from "./components/AppAlert";
 
 export default {
     name: "App",
     components: {
-        AppHeader
+        AppHeader,
+        AppAlert,
     },
 }
 </script>
---
+++ resources/js/components/AppAlert.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="px-1 alert-container">
+        <v-alert
+            v-for="(alert, index) in alerts" :key="index"
+            :type="alert.type"
+            width="100%"
+            dismissible
+            class="mb-1"
+        >
+            {{ alert.message }}
+        </v-alert>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppAlert",
+    data: function() {
+        return {
+            alerts: [
+                {
+                    type: "info",
+                    message: "Information",
+                },
+                {
+                    type: "error",
+                    message: "Error",
+                }
+            ],
+        }
+    }
+}
+</script>
+
+<style scoped>
+.alert-container {
+    position: absolute;
+    z-index: 100;
+    width: 100%;
+}
+</style>
